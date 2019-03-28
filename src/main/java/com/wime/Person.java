a265f84ff9b02a6e0cc83013784f4227f78fd437
mausaki <rainna.saki@gmail.com>
2019-03-28 16:55:46 +0800

    one

---
+++ src/main/java/com/wime/Person.java
@@ -0,0 +1,8 @@
+package com.wime;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/main/kotlin/com/wime/Hello.kt
@@ -0,0 +1,14 @@
+package com.wime
+
+fun main(arg: Array<String>){
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h=Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
