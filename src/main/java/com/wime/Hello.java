b237fdeee7ea14013fd2d52691febe04398c436f
mausaki <rainna.saki@gmail.com>
2019-03-28 17:13:34 +0800

    java variable change.

--- src/main/java/com/wime/Hello.java
+++ src/main/java/com/wime/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //        System.out.println("Hello World");
         Person p =new Person();
         p.hello();
+        int age = 19;
+        Integer age2=19;
+        if (age2.equals(19)){
+            System.out.println("true");
+        }else {
+            System.out.println("f");
+        }
+        float weight=66.5f;
+        String name="Tom";
+
     }
 }
